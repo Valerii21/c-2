5b0d67e92ba8390baee18b726513ff71b9b0b841
Valerii21 <109736919+Valerii21@users.noreply.github.com>
2022-08-07 13:14:08 +0300

    2

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿ Console.WriteLine("Введите 3 числа:");
+            int num_1 = Convert.ToInt32(Console.ReadLine());
+            int num_2 = Convert.ToInt32(Console.ReadLine());
+            int num_3 = Convert.ToInt32(Console.ReadLine());
+
+            int max = num_1;
+
+            if (num_2 > max)
+            {
+                max = num_2;
+            }
+
+            if (num_3 > max)
+            {
+                max = num_3;
+            }
+
+            Console.WriteLine("Наибольшее из введённых чисел -> " + max);
